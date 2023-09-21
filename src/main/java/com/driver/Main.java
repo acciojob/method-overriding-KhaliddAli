3c7ad4cdcfa0c778799185305f63156f76617f35
khalid <khalidali967@gmail.com>
2023-09-21 14:16:33 +0530

    first attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+        B obj = new B();
+        System.out.println(obj.meth());
+
+        System.out.println(obj.meth());
+    }
+
+
 }
